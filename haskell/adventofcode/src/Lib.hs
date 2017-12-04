0a8aa39c0534636c77822282fac3c1a42f31f556
Hussein Ait Lahcen <hussein.aitlahcen@cgi.com>
2017-12-04 16:12:20 +0000

    Fix: adventofcode: added part1

--- haskell/adventofcode/src/Lib.hs
+++ haskell/adventofcode/src/Lib.hs
@@ -5,23 +5,27 @@ import qualified Data.Set as S
 import qualified Data.ByteString.Char8 as C
 import qualified Data.ByteString as BS
 
-contentOf :: String -> IO BS.ByteString
+contentOf :: String
+          -> IO BS.ByteString
 contentOf = BS.readFile . ("./files/" ++)
 
-isValid :: BS.ByteString -> Bool
-isValid line = length original == (S.size . uniques) original
+isValid :: ([BS.ByteString] -> [BS.ByteString])
+        -> BS.ByteString
+        -> Bool
+isValid projection line = length original == (S.size . uniques) original
   where
     words = C.split ' '
-    sorted = map BS.sort
-    original = sorted . words $ line
+    original = projection . words $ line
     uniques = S.fromList
 
-day4 :: BS.ByteString -> Int
-day4 content = length . valid . nonEmpty . lines $ content
+day4 :: BS.ByteString -> (Int, Int)
+day4 content = (part1, part2)
   where
-    valid = filter isValid
     lines = C.split '\n'
     nonEmpty = filter (/= BS.empty)
+    validLength f = length . filter (isValid f) . nonEmpty . lines $ content
+    part1 = validLength id
+    part2 = validLength (map BS.sort)
 
 run :: IO ()
 run = day4 <$> contentOf "day4.txt"  >>= print
