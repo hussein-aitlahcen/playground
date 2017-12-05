8a8df3dd1a9b0f202bf818970221e7791851c3e4
Hussein Ait Lahcen <hussein.aitlahcen@cgi.com>
2017-12-05 11:08:34 +0000

    Add: Starting day 5, refatored

--- haskell/adventofcode/src/Lib.hs
+++ haskell/adventofcode/src/Lib.hs
@@ -1,31 +1,38 @@
 module Lib where
 
+import Control.Monad
 import Data.List
 import qualified Data.Set as S
 import qualified Data.ByteString.Char8 as C
 import qualified Data.ByteString as BS
 
+type Level = Input -> Output
+type Input = BS.ByteString
+type Output = (Int, Int)
+
 contentOf :: String
-          -> IO BS.ByteString
+          -> IO Input
 contentOf = BS.readFile . ("./files/" ++)
 
-isValid :: ([BS.ByteString] -> [BS.ByteString])
-        -> BS.ByteString
-        -> Bool
-isValid projection line = length original == (S.size . uniques) original
-  where
-    words = C.split ' '
-    original = projection . words $ line
-    uniques = S.fromList
-
-day4 :: BS.ByteString -> (Int, Int)
+day4 :: Level
 day4 content = (part1, part2)
   where
     lines = C.split '\n'
     nonEmpty = filter (/= BS.empty)
     validLength f = length . filter (isValid f) . nonEmpty . lines $ content
+    isValid projection line = length original == (S.size . uniques) original
+      where
+        words = C.split ' '
+        original = projection . words $ line
+        uniques = S.fromList
     part1 = validLength id
     part2 = validLength (map BS.sort)
 
+day5 :: Level
+day5 content = (0, 0)
+
+days :: [(String, Level)]
+days = [("day4", day4), ("day5", day5)]
+
 run :: IO ()
-run = day4 <$> contentOf "day4.txt"  >>= print
+run = join $ mapM_ putStrLn <$> mapM (\(n, f) -> ((++) (n ++ " -> ") . show) . f <$> contentOf (n ++ ".txt")) days
