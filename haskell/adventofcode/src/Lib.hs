50403df4009c004b7cdd6441329e05c2cce1da3c
Hussein Ait Lahcen <hussein.aitlahcen@cgi.com>
2017-12-07 17:27:43 +0000

    Add: haskell: adventofcode: day6/7

--- haskell/adventofcode/src/Lib.hs
+++ haskell/adventofcode/src/Lib.hs
@@ -1,15 +1,22 @@
 {-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Lib where
 
-import qualified Data.Sequence as SQ
+import Control.Applicative
 import Control.Monad
+import Data.Char
 import Data.List
+import Data.Attoparsec.ByteString
+import Data.Attoparsec.ByteString.Char8 as AC
+import Data.Attoparsec.Combinator
+import qualified Data.Sequence as SQ
 import qualified Data.Set as S
 import qualified Data.ByteString.Char8 as C
+import qualified Data.ByteString.Internal as BSI
 import qualified Data.ByteString as BS
 
-type Level = Input -> Output
+type Level = Input -> IO Output
 type Input = BS.ByteString
 type Output = (Int, Int)
 
@@ -27,7 +34,7 @@ day4 :: Level
 day4 content = let part1 = validLength id
                    part2 = validLength $ map BS.sort
                in
-                 (part1, part2)
+                 return (part1, part2)
   where
     validLength f = length . filter (isValid f) . nonEmptyLines $ content
     isValid projection line = length original == (S.size . uniques) original
@@ -40,7 +47,7 @@ day5 :: Level
 day5 content = let part1 = run id 0 0 maze
                    part2 = run  (\x -> if x >= 3 then -1 else 1) 0 0 maze
                in
-                 (part1, part2)
+                 return (part1, part2)
   where
     maze = SQ.fromList . map (readInt . C.unpack) . nonEmptyLines $ content
     readInt x = read x :: Int
@@ -54,8 +61,31 @@ day5 content = let part1 = run id 0 0 maze
       where
         sl = SQ.length
 
+day6 :: Level
+day6 content = return (0, 0)
+
+data FlatTree = FlatTree { identifier :: BS.ByteString, weight :: Int, childrends :: [BS.ByteString] } deriving (Show)
+
+-- ssqhzgo (183) -> idwscr, jwmobb
+day7 :: Level
+day7 content = let x = mapM tree . nonEmptyLines $ content
+               in case x of
+                    Right trees -> print trees >> return (0, 0)
+                    Left err -> error err
+  where
+    readIdent = AC.takeWhile1 isLetter
+    readWeight = char '(' *> decimal <* char ')'
+    readChilds = AC.takeWhile (not . isLetter) *> AC.takeWhile isLetter `AC.sepBy` string ", "
+    readTree = do
+      ident <- readIdent
+      char ' '
+      w <- readWeight
+      childs <- readChilds
+      return $ FlatTree ident w childs
+    tree = parseOnly readTree
+
 days :: [(String, Level)]
-days = [("day4", day4), ("day5", day5)]
+days = [("day7", day7)]
 
 run :: IO ()
-run = join $ mapM_ putStrLn <$> mapM (\(n, f) -> ((++) (n ++ " -> ") . show) . f <$> contentOf (n ++ ".txt")) days
+run = join $ mapM_ putStrLn <$> mapM (\(n, f) -> ((++) (n ++ " -> ") . show <$>) . f =<< contentOf (n ++ ".txt")) days
