8640d86beff89974064d750a99021260630829fb
Hussein Ait Lahcen <hussein.aitlahcen@cgi.com>
2017-12-08 17:48:42 +0000

    Add: haskell: adventofcode: day8

--- haskell/adventofcode/src/Lib.hs
+++ haskell/adventofcode/src/Lib.hs
@@ -8,14 +8,13 @@ import Control.Monad
 import Data.Char
 import Data.List
 import qualified Data.Map.Strict as M
-import Data.Attoparsec.ByteString
 import Data.Attoparsec.ByteString.Char8 as AC
 import Data.Attoparsec.Combinator
 import qualified Data.Sequence as SQ
 import qualified Data.Set as S
 import qualified Data.ByteString.Char8 as C
 import qualified Data.ByteString.Internal as BSI
-import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BS
 
 type Level = Input -> IO Output
 type Input = BS.ByteString
@@ -85,7 +84,7 @@ day7 content = let x = mapM tree . nonEmptyLines $ content
   where
     readIdent = AC.takeWhile1 isLetter
     readWeight = char '(' *> decimal <* char ')'
-    readChilds = AC.takeWhile (not . isLetter) *> AC.takeWhile isLetter `AC.sepBy` string ", "
+    readChilds = AC.takeWhile (not . isLetter) *> AC.takeWhile isLetter `sepBy` string ", "
     readTree = do
       ident <- readIdent
       char ' '
@@ -94,8 +93,63 @@ day7 content = let x = mapM tree . nonEmptyLines $ content
       return $ FlatTree ident w childs
     tree = parseOnly readTree
 
+data Instruction = Instruction { register :: BS.ByteString, method :: Method, methodValue :: Int, condition :: Condition } deriving Show
+data Method = Inc | Dec deriving (Show)
+data Condition = Condition { targetReg :: BS.ByteString, operator :: Operator, checkValue :: Int } deriving Show
+data Operator = G | L | GE | LE | E | NE deriving Show
+-- osr inc -350 if jpc <= -3271
+-- pv dec -885 if pv < -2563
+-- jpc dec -224 if g == 1359
+-- g dec 793 if gx != -1557
+-- k inc -874 if v == 366
+day8 :: Level
+day8 content = do
+  let (Right i) = mapM readInstruction . nonEmptyLines $ content
+  print $ interpret M.empty i 0
+  return (0, 0)
+  where
+    readValue = signed decimal
+    readRegister = AC.takeWhile isLetter
+    readMethod =
+      ("inc" >> return Inc) <|>
+      ("dec" >> return Dec)
+    readOperator =
+      (">=" >> return GE) <|>
+      ("<=" >> return LE) <|>
+      ("==" >> return E) <|>
+      ("!=" >> return NE) <|>
+      (">" >> return G) <|>
+      ("<" >> return L)
+    readCondition = Condition <$> ("if" *> skipSpace *> readRegister) <* skipSpace <*> readOperator <* skipSpace <*> readValue
+    readInstruction = parseOnly $ Instruction <$> readRegister <* skipSpace <*> readMethod <* skipSpace <*> readValue <* skipSpace <*> readCondition
+    interpret !regs !stack !i
+      | i >= length stack = maximum . M.elems $ regs
+      | otherwise = let (Instruction ir im imv ic) = stack !! i
+                        f = case im of
+                          Inc -> (+)
+                          Dec -> (-)
+                        nextValue x = case x of
+                          Just y -> Just $ f y imv
+                          Nothing -> Just $ f 0 imv
+                        nextIndex = i + 1
+                    in
+                      if validCondition ic then
+                        interpret (M.alter nextValue ir regs) stack nextIndex
+                      else
+                        interpret regs stack nextIndex
+      where
+        validCondition (Condition reg op checkValue) = case op of
+                                                         G -> rv > checkValue
+                                                         L -> rv < checkValue
+                                                         GE -> rv >= checkValue
+                                                         LE -> rv <= checkValue
+                                                         E -> rv == checkValue
+                                                         NE -> rv /= checkValue
+            where
+              rv = M.findWithDefault 0 reg regs
+
 days :: [(String, Level)]
-days = [("day7", day7)]
+days = [("day8", day8)]
 
 run :: IO ()
 run = join $ mapM_ putStrLn <$> mapM (\(n, f) -> ((++) (n ++ " -> ") . show <$>) . f =<< contentOf (n ++ ".txt")) days
