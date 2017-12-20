592dde7cbf7d817aaa8d4b362d1c2dc7675ec4da
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2017-12-20 10:27:38 +0000

    Add: haskell/curious: messing with hspec

--- haskell/curious/test/Spec.hs
+++ haskell/curious/test/Spec.hs
@@ -1,2 +1,33 @@
+-- Spec.hs ---
+
+-- Copyright (C) 2017 Hussein Ait-Lahcen
+
+-- Author: Hussein Ait-Lahcen <hussein.aitlahcen@gmail.com>
+
+-- This program is free software; you can redistribute it and/or
+-- modify it under the terms of the GNU General Public License
+-- as published by the Free Software Foundation; either version 3
+-- of the License, or (at your option) any later version.
+
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+
+-- You should have received a copy of the GNU General Public License
+-- along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
+import Lib
+import Test.Hspec
+import Test.QuickCheck
+import Control.Exception (evaluate)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "function f" $ do
+    it "add one and double the value" $ do
+      runF f 0 `shouldBe` 3
+  describe "function h" $ do
+    it "compose twice f" $ do
+      runF h 0 `shouldBe` 6
