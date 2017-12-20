5153431d716e034536be2a1ebbff47e65829ab62
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2017-12-20 10:23:39 +0000

    Add: license header

--- haskell/curious/src/Lib.hs
+++ haskell/curious/src/Lib.hs
@@ -1,3 +1,23 @@
+-- Lib.hs ---
+
+-- Copyright (C) 2017 Hussein Ait-Lahcen
+
+-- Author: Hussein Ait-Lahcen <hussein.aitlahcen@gmail.com>
+
+-- This program is free software; you can redistribute it and/or
+-- modify it under the terms of the GNU General Public License
+-- as published by the Free Software Foundation; either version 3
+-- of the License, or (at your option) any later version.
+
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+
+-- You should have received a copy of the GNU General Public License
+-- along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
 {-# LANGUAGE Arrows #-}
 
 module Lib where
@@ -18,7 +38,7 @@ instance Category Pipe where
   (Pipe g) . (Pipe f) = Pipe (g . f)
   id = arr id
 
-f, h:: Pipe Int Int
+f, h :: Pipe Int Int
 f = arr $ (* 3) . (+ 1)
 h = f &&& f >>> (arr . uncurry) (+)
 
